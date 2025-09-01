4127bc07c534c9c7b8c49f671ff6ef4b299ab091
Mehmet Durmaz <mehmetdurmaz@gmail.com>
2025-09-01 22:08:01 +0300

    Add scheduled job statistics model

--- IYSIntegration.Application/Services/ScheduledMultipleConsentQueryService.cs
+++ IYSIntegration.Application/Services/ScheduledMultipleConsentQueryService.cs
@@ -21,8 +21,9 @@ namespace IYSIntegration.Application.Services
             _consentService = consentService;
         }
 
-        public async Task RunAsync(int batchCount)
+        public async Task<ResponseBase<ScheduledJobStatistics>> RunAsync(int batchCount)
         {
+            var response = new ResponseBase<ScheduledJobStatistics>();
             bool errorFlag = false;
             int successCount = 0;
             int failedCount = 0;
@@ -98,6 +99,13 @@ namespace IYSIntegration.Application.Services
 
             if (errorFlag)
                 _logger.LogError($"MultipleConsentQueryService toplam {failedCount + successCount} içinden {failedCount} recipient için hata aldı, IYSConsentRequest tablosuna bakın");
+
+            response.Data = new ScheduledJobStatistics { SuccessCount = successCount, FailedCount = failedCount };
+            if (errorFlag)
+            {
+                response.Error("MULTIPLE_CONSENT_QUERY", "One or more batches failed.");
+            }
+            return response;
         }
     }
 }
