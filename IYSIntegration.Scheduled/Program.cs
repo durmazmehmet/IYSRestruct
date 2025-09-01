92be7191185fac8899b871cc2359eb4cf25561fd
Mehmet DURMAZ <medurmaz@borusan.com>
2025-09-01 12:47:58 +0300

    Paketlerin düzenlenmesi

--- IYSIntegration.Scheduled/Program.cs
+++ IYSIntegration.Scheduled/Program.cs
@@ -1,8 +1,9 @@
-using IYSIntegration.Application.Interface;
+﻿using IYSIntegration.Application.Interface;
 using IYSIntegration.Application.Services;
 using IYSIntegration.Common.Base;
 using IYSIntegration.Common.LoggingService;
 using IYSIntegration.Common.LoggingService.Loggers;
+using IYSIntegration.Common.Middleware.Exceptions;
 using IYSIntegration.Common.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
@@ -10,36 +11,61 @@ using Microsoft.Extensions.Options;
 using Serilog;
 using StackExchange.Redis;
 
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Host.UseSerilog((ctx, lc) => lc.ReadFrom.Configuration(ctx.Configuration));
-
-builder.Services.Configure<CacheSettings>(builder.Configuration.GetSection("CacheSettings"));
-builder.Services.AddSingleton<IConnectionMultiplexer>(sp =>
+internal class Program
 {
-    var cacheSettings = sp.GetRequiredService<IOptions<CacheSettings>>().Value;
-    return ConnectionMultiplexer.Connect(cacheSettings.ConnectionString);
-});
-builder.Services.AddSingleton<ICacheService, CacheService>();
-builder.Services.AddSingleton<LoggerServiceBase>(_ => new GrayLogger());
-builder.Services.AddSingleton<IDbService, DbService>();
-builder.Services.AddSingleton<IIdentityService, IdentityService>();
-builder.Services.AddSingleton<ISfIdentityService, SfIdentityService>();
-builder.Services.AddSingleton<IRestClientService, RestClientService>();
-builder.Services.AddSingleton<IConsentService, ConsentService>();
-builder.Services.AddSingleton<ISfConsentService, SfConsentService>();
-builder.Services.AddSingleton<IIntegrationService, IntegrationService>();
-builder.Services.AddScoped<MultipleConsentQueryService>();
-builder.Services.AddScoped<SingleConsentAddService>();
-builder.Services.AddScoped<MultipleConsentAddService>();
-builder.Services.AddScoped<PullConsentService>();
-builder.Services.AddScoped<SfConsentScheduledService>();
-builder.Services.AddScoped<SendConsentErrorService>();
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+        // 1) Logging
+        builder.Host.UseSerilog((ctx, lc) => lc.ReadFrom.Configuration(ctx.Configuration));
 
-var app = builder.Build();
+        // 2) Options & Config
+        builder.Services.Configure<CacheSettings>(builder.Configuration.GetSection("CacheSettings"));
 
-app.MapControllers();
+        // 3) Infrastructure singletons
+        builder.Services.AddSingleton<IConnectionMultiplexer>(sp =>
+        {
+            var cacheSettings = sp.GetRequiredService<IOptions<CacheSettings>>().Value;
+            return ConnectionMultiplexer.Connect(cacheSettings.ConnectionString);
+        });
+        builder.Services.AddSingleton<ICacheService, CacheService>();
 
-app.Run();
+        // 4) Cross-cutting services
+        builder.Services.AddSingleton<LoggerServiceBase>(_ => new GrayLogger());
+
+        // 5) Domain/Application services
+        builder.Services.AddSingleton<IDbService, DbService>();
+        builder.Services.AddSingleton<IIdentityService, IdentityService>();
+        builder.Services.AddSingleton<ISfIdentityService, SfIdentityService>();
+        builder.Services.AddSingleton<IRestClientService, RestClientService>();
+        builder.Services.AddSingleton<IConsentService, ConsentService>();
+        builder.Services.AddSingleton<ISfConsentService, SfConsentService>();
+        builder.Services.AddSingleton<IIntegrationService, IntegrationService>();
+
+        // Scoped application workflows
+        builder.Services.AddScoped<MultipleConsentQueryService>();
+        builder.Services.AddScoped<SingleConsentAddService>();
+        builder.Services.AddScoped<MultipleConsentAddService>();
+        builder.Services.AddScoped<PullConsentService>();
+        builder.Services.AddScoped<SfConsentScheduledService>();
+        builder.Services.AddScoped<SendConsentErrorService>();
+
+        // 6) MVC & Swagger
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        // 7) Middleware pipeline
+        app.UseMiddleware<ExceptionMiddleware>();     // En üste: tüm hataları yakalasın
+
+        app.UseSwagger();
+        app.UseSwaggerUI();
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
