eaa9bbf0615d6ad0ad31437356bd09a0418c9c0b
Mehmet Durmaz <mehmetdurmaz@gmail.com>
2025-09-18 13:40:38 +0300

    Fix duplicate and pending sync service return types

--- IYSIntegration.Application/Services/PendingSyncService.cs
+++ IYSIntegration.Application/Services/PendingSyncService.cs
@@ -1,4 +1,5 @@
 using IYSIntegration.Application.Services.Interface;
+using IYSIntegration.Application.Services.Models;
 using IYSIntegration.Application.Services.Models.Base;
 using IYSIntegration.Application.Services.Models.Request.Consent;
 using IYSIntegration.Application.Services.Models.Response.Consent;
