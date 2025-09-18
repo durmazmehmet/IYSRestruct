8cd1c8b770924f2e3759a6ca3c09fb58dbb25380
Mehmet Durmaz <mehmetdurmaz@gmail.com>
2025-09-18 13:34:51 +0300

    Remove addMultipleConsentV2 endpoints

--- IYSIntegration.Application/Services/Interface/IIysHelper.cs
+++ IYSIntegration.Application/Services/Interface/IIysHelper.cs
@@ -1,4 +1,7 @@
-﻿using IYSIntegration.Application.Services.Models.Base;
+using IYSIntegration.Application.Services.Models;
+using IYSIntegration.Application.Services.Models.Base;
+using IYSIntegration.Application.Services.Models.Request.Consent;
+using IYSIntegration.Application.Services.Models.Response.Consent;
 
 namespace IYSIntegration.Application.Services.Interface;
 
@@ -8,4 +11,30 @@ public interface IIysHelper
     string? GetCompanyCode(int code);
     bool IsOlderThanBusinessDays(DateTime consentDate, int maxBusinessDays);
     List<string> GetAllCompanyCodes();
+    Task<(bool IsValid, ResponseBase<AddConsentResult> Response)> ValidateConsentRequestAsync(
+        AddConsentRequest request,
+        IDbService dbService);
+
+    Task<List<ConsentProcessingResult>> ValidateMultipleConsentsAsync(
+        MultipleConsentRequest request,
+        IDbService dbService);
+
+    void AppendValidationMessages(
+        ResponseBase<MultipleConsentResult> response,
+        ConsentProcessingResult consentResult);
+
+    Task LogConsentAsync(
+        AddConsentRequest request,
+        ResponseBase<AddConsentResult> response,
+        IDbService dbService,
+        IDuplicateCleanerService duplicateCleanerService,
+        IPendingSyncService pendingSyncService);
+
+    Task<int> LogConsentRequestAsync(
+        AddConsentRequest request,
+        IDbService dbService,
+        IDuplicateCleanerService duplicateCleanerService,
+        IPendingSyncService pendingSyncService);
+
+    string? ResolveCompanyCode(string? companyCode, string? companyName, int iysCode);
 }
