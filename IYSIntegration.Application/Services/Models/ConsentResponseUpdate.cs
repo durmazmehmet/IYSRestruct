1b37e7ff3c4216220947ba44b15b58727cfe71e8
Mehmet Durmaz <mehmetdurmaz@gmail.com>
2025-09-19 00:03:02 +0300

    Refactor consent response handling to service

---
+++ IYSIntegration.Application/Services/Models/ConsentResponseUpdate.cs
@@ -0,0 +1,13 @@
+namespace IYSIntegration.Application.Services.Models
+{
+    public class ConsentResponseUpdate
+    {
+        public long Id { get; set; }
+        public long LogId { get; set; }
+        public bool IsSuccess { get; set; }
+        public string? TransactionId { get; set; }
+        public string? CreationDate { get; set; }
+        public string? BatchError { get; set; }
+        public bool IsOverdue { get; set; }
+    }
+}
